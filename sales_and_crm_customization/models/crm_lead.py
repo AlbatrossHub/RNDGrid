3c070cd83e87f69c1bfe84535a9ecce28f04ab6f
hardikpy <hardik.patel8@appinventiv.com>
2026-03-12 19:35:01 +0530

    correct test mapping

--- sales_and_crm_customization/models/crm_lead.py
+++ sales_and_crm_customization/models/crm_lead.py
@@ -4,26 +4,11 @@ from odoo import models, fields
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    rndgrid_material_name = fields.Char(string='Material Name', Tracking=True)
     rndgrid_segment = fields.Selection(
         related='partner_id.rndgrid_segment',
         string='User Type',
         readonly=True,
     )
-    rndgrid_sample_type = fields.Selection([
-        ('liquid', 'Liquid'),
-        ('solid', 'Solid'),
-        ('powder', 'Powder'),
-        ('thin_film', 'Thin Film'),
-        ('nanomaterial', 'NanoMaterial'),
-        ('biological', 'Biological Sample'),
-        ('polymer', 'Polymer / Plastic'),
-        ('metal', 'Metal/Alloy'),
-        ('composite', 'Composite / Ceramic'),
-        ('coated_surface', 'Coated Surface'),
-        ('other', 'Other')
-    ], string='Sample Type', Tracking=True)
-    rndgrid_is_hazardous = fields.Boolean(string='Is this sample hazardous?', default=False, Tracking=True)
     rndgrid_test_line_ids = fields.One2many(
         'crm.lead.rndgrid.test', 'lead_id', 
         string='Requested Tests'
@@ -68,3 +53,19 @@ class CrmLeadRndGridTest(models.Model):
         domain="[('instrument_id', '=', instrument_id)]",
         help="Which specific tests are required using this instrument?"
     )
+    
+    rndgrid_material_name = fields.Char(string='Material Name')
+    rndgrid_sample_type = fields.Selection([
+        ('liquid', 'Liquid'),
+        ('solid', 'Solid'),
+        ('powder', 'Powder'),
+        ('thin_film', 'Thin Film'),
+        ('nanomaterial', 'NanoMaterial'),
+        ('biological', 'Biological Sample'),
+        ('polymer', 'Polymer / Plastic'),
+        ('metal', 'Metal/Alloy'),
+        ('composite', 'Composite / Ceramic'),
+        ('coated_surface', 'Coated Surface'),
+        ('other', 'Other')
+    ], string='Sample Type')
+    rndgrid_is_hazardous = fields.Boolean(string='hazardous?', default=False)
